dc41fd1b4c5c874d2cf6633194fff80a9893539a
satyamjain-cpu <113338521+satyamjain-cpu@users.noreply.github.com>
2023-01-01 14:12:21 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,13 +1,16 @@
-
+/* eslint-disable require-jsdoc */
 'use strict';
 const {
   Model, Op
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-   
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate() {
-      // define association here
     }
 
     static addTodo({title, dueDate}) {
@@ -22,50 +25,31 @@ module.exports = (sequelize, DataTypes) => {
       return await Todo.findAll({
         where: {
           dueDate: { [Op.lt]: new Date().toLocaleDateString("en-CA") },
-          completed: false,
         },
       });
     }
 
     static async dueToday() {
-      // FILL IN HERE TO RETURN ITEMS DUE tODAY
       return await Todo.findAll({
         where: {
           dueDate: { [Op.eq]: new Date().toLocaleDateString("en-CA") },
-          completed: false,
         },
       });
     }
 
     static async dueLater() {
-      // FILL IN HERE TO RETURN ITEMS DUE LATER
       return await Todo.findAll({
         where: {
           dueDate: { [Op.gt]: new Date().toLocaleDateString("en-CA") },
-          completed: false,
         },
       });
     }
 
-    static async remove(id) {
-      return this.destroy({
-        where: {
-          id,
-        }
-      })
-    }
 
-    static async completedItems(){
-      return this.findAll({
-        where: {
-          completed: true,
-        }
-      })
+
+    markAsCompleted() {
+      return this.update({completed: true});
     }
-    setCompletionStatus(receiver) {
-      return this.update({ completed: receiver });
-    }
-    
   }
   Todo.init({
     title: DataTypes.STRING,
